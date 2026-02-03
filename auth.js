62760416b73f489d1bb215db74517c9d507783b1
Vaniang <116418019+Vaniang@users.noreply.github.com>
2026-02-03 17:19:18 +0800

    auth.js

--- auth.js
+++ auth.js
@@ -27,7 +27,7 @@ function login() {
                 if(doc.exists) {
                     const role = doc.data().role;
                     if(role === 'admin') {
-                        window.location.href = "admin/admin.html";
+                        window.location.href = "admin.html";
                     } else {
                         window.location.href = "user/user.html";
                     }
